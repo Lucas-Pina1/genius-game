eefa49b6317546d73d044ef2ec825fb73ea920c1
Lucas Pina <ti.lucaspina@gmail.com>
2021-10-04 13:32:03 -0300

    continuaçao do Javascript

--- js/script.js
+++ js/script.js
@@ -12,6 +12,7 @@ const red = document.querySelector('.red');
 const yellow= document.querySelector('.yellow');
 const blue = document.querySelector('.blue');
 
+//cria ordem aleatória das cores
 let shuffleOrder = () => {
   let colorOrder = Math.floor(Math.random() * 4);
   order[order.length] = colorOrder;
@@ -23,12 +24,40 @@ let shuffleOrder = () => {
   }
 }
 
+//acende a proxima cor
 let lightColor = (element, number) => {
   number = number * 500;
   setTimeout(() => {
       element.classList.add('selected');
   }, number - 250);
   setTimeout(() => {
-      element.classList.remove("selected");
-  }, timeout);
+      element.classList.remove('selected');
+  });
+}
+
+
+//checa se os botões clicados são os mesmos da ordem gerada pelo jogo 
+let checkOrder = () => {
+  for(let i in clickedOrder) {
+    if(clickedOrder[i] != order[i]) {
+      lose();
+      break;
+    }
+  }
+  if(clickedOrder.length == order.length) {
+    alert(`Pontuação: ${score}\n Você acertou! Iniciando o proximo nivél!`);
+    nextLevel();
+  }
+}
+
+//função para o clique do usuário 
+let click = (color) => {
+  clickedOrder[clickedOrder.length] = color; 
+  createColorElement(color).classList.add('selected');
+
+  setTimeout(() => {
+    createColorElement(color).classList.remove('selected');
+  })
+
+  checkOrder();
 }
